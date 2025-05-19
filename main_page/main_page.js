f2357c2ac6a56e7ce676f39684aad9961f573455
GodWin <p8866857@gmail.com>
2025-05-20 02:01:31 +0300

    -feat: main page slider

--- main_page/main_page.js
+++ main_page/main_page.js
@@ -11,6 +11,54 @@ document.getElementById('view-all-products-btn').addEventListener('click', funct
 window.location.href = '/shop_page/shop_page.html';
 });
 
+//слайдер
+  document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.getElementById('testimonial-slider');
+    const prevBtn = document.getElementById('prev-testimonial');
+    const nextBtn = document.getElementById('next-testimonial');
+
+    let comments = [];
+    let currentIndex = 0;
+
+    function renderComment(comment) {
+      slider.innerHTML = `
+        <section class="fifth-block-second-section-overlay">
+          <img src="${comment['profile-image']}" class="fifth-block-woman-image" alt="User Image">
+          <img src="./assets/five_star_2.png" class="fifth-block-five-star-image" alt="five_star_image">
+          <p class="fifth-block-quote">${comment.quote}</p>
+          <h3>${comment.nickname}</h3>
+          <p class="fifth-block-consumer">${comment.status}</p>
+        </section>
+      `;
+    }
+
+    function showComment(index) {
+      if (comments.length > 0) {
+        renderComment(comments[index]);
+      }
+    }
+
+    fetch('http://localhost:3000/comments')
+      .then(response => response.json())
+      .then(data => {
+        comments = data;
+        showComment(currentIndex);
+      })
+      .catch(error => {
+        console.error('Ошибка загрузки комментариев:', error);
+      });
+
+    nextBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % comments.length;
+      showComment(currentIndex);
+    });
+
+    prevBtn.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + comments.length) % comments.length;
+      showComment(currentIndex);
+    });
+  });
+
 // Загружаем header
 fetch('/templates/header.html')
     .then(response => response.text())
